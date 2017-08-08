66d74a9b3255f548d71048db85dbea1a1ce592e4
maxawolff <maxawolff@hotmail.com>
2017-08-08 15:38:27 -0700

    Started on the app.js file by adding the use strict message and added 5 variables to serve as the questions that will be asked to the user

---
+++ app.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var question1 = 'Question 1. Do I own any pets?';
+var question2 = 'Question 2. Was I born in Washington State?';
+var question3 = 'Question 3. Have I ever coded before joining Code Fellows?';
+var question4 = 'Question 4. Have I ever been outside of the country?';
+var question5 = 'Question 5. Have I ever been to a live concert?';
