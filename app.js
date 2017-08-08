31e3101ed9dd799f11101f3c553a459360f859bf
maxawolff <maxawolff@hotmail.com>
2017-08-08 16:29:49 -0700

    used the same code for recieving and checking an answer for question 1, and applied it to the questions 2-5

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var correctResponse = 'Yep, That\'s right!';
 var incorrectResponse = 'Nope, that\'s not right';
 
 alert('Hello there, this is my about me page. Were going to play a little game so that you can learn more about me. Please answer the following 5 questions in a yes or no fashion.');
+//the following block of code gets repeated five times (there are slight changes depending on whether or not the answer is true or false) I reckognize that this would work better as a function but since we haven't learned that yet I will just have the code coppied.
 var answer1 = prompt(question1);
 console.log('Asked the user the following question: ' + question1 + ' And they responded with ' + answer1);
 answer1 = answer1.toUpperCase();
@@ -20,6 +21,74 @@ if(answer1 === 'YES' || answer1 === 'Y'){
 else if (answer1 === 'NO' || answer1 === 'N') {
   alert(correctResponse);
   console.log('They got the right answer');
+}
+else{
+  alert('It looks like your answer was not a yes or no response, please answer with a yes or no next time');
+  console.log('They answered improperly, the answer needed to be a yes or no');
+}
+
+var answer2 = prompt(question2);
+console.log('Asked the user the following question: ' + question2 + ' And they responded with ' + answer2);
+answer2 = answer2.toUpperCase();
+console.log('Changed their answer to ' + answer2 + ' in order to check if they provided a valid answer');
+if(answer2 === 'YES' || answer2 === 'Y'){
+  alert(incorrectResponse);
+  console.log('They got the wrong answer');
+}
+else if (answer2 === 'NO' || answer2 === 'N') {
+  alert(correctResponse);
+  console.log('They got the right answer');
+}
+else{
+  alert('It looks like your answer was not a yes or no response, please answer with a yes or no next time');
+  console.log('They answered improperly, the answer needed to be a yes or no');
+}
+
+var answer3 = prompt(question3);
+console.log('Asked the user the following question: ' + question3 + ' And they responded with ' + answer3);
+answer3 = answer3.toUpperCase();
+console.log('Changed their answer to ' + answer3 + ' in order to check if they provided a valid answer');
+if(answer3 === 'YES' || answer3 === 'Y'){
+  alert(correctResponse);
+  console.log('They got the right answer');
+}
+else if (answer3 === 'NO' || answer3 === 'N') {
+  alert(incorrectResponse);
+  console.log('They got the wrong answer');
+}
+else{
+  alert('It looks like your answer was not a yes or no response, please answer with a yes or no next time');
+  console.log('They answered improperly, the answer needed to be a yes or no');
+}
+
+var answer4 = prompt(question4);
+console.log('Asked the user the following question: ' + question4 + ' And they responded with ' + answer4);
+answer4 = answer4.toUpperCase();
+console.log('Changed their answer to ' + answer4 + ' in order to check if they provided a valid answer');
+if(answer4 === 'YES' || answer4 === 'Y'){
+  alert(correctResponse);
+  console.log('They got the right answer');
+}
+else if (answer4 === 'NO' || answer4 === 'N') {
+  alert(incorrectResponse);
+  console.log('They got the wrong answer');
+}
+else{
+  alert('It looks like your answer was not a yes or no response, please answer with a yes or no next time');
+  console.log('They answered improperly, the answer needed to be a yes or no');
+}
+
+var answer5 = prompt(question5);
+console.log('Asked the user the following question: ' + question5 + ' And they responded with ' + answer5);
+answer5 = answer5.toUpperCase();
+console.log('Changed their answer to ' + answer5 + ' in order to check if they provided a valid answer');
+if(answer5 === 'YES' || answer5 === 'Y'){
+  alert(correctResponse);
+  console.log('They got the right answer');
+}
+else if (answer5 === 'NO' || answer5 === 'N') {
+  alert(incorrectResponse);
+  console.log('They got the wrong answer');
 }
 else{
   alert('It looks like your answer was not a yes or no response, please answer with a yes or no next time');
