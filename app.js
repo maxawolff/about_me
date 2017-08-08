a805a8695d325706a6d2f24bf5c261a4d4b4b7f8
maxawolff <maxawolff@hotmail.com>
2017-08-08 15:48:44 -0700

    added the first prompt for the user to answer a question and then changed it to be uppercase. Then I added two logs to the console for debugging purposes.

--- app.js
+++ app.js
@@ -5,3 +5,9 @@ var question2 = 'Question 2. Was I born in Washington State?';
 var question3 = 'Question 3. Have I ever coded before joining Code Fellows?';
 var question4 = 'Question 4. Have I ever been outside of the country?';
 var question5 = 'Question 5. Have I ever been to a live concert?';
+
+alert('Hello there, this is my about me page. Were going to play a little game so that you can learn more about me. Please answer the following 5 questions in a yes or no fashion.');
+var answer1 = prompt(question1);
+console.log('Asked the user the following question ' + question1 + ' and they responded with ' + answer1);
+answer1 = answer1.toUpperCase();
+console.log('changed their answer to ' + answer1 + ' in order to check if they provided a valid answer');
